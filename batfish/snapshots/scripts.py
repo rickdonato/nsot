15c28e3703a78229076f7b875fb0488d3d76cc4d
Rick Donato <rickj07@gmail.com>
2019-06-18 14:15:14 +0100

    Update scripts.py

--- batfish/snapshots/scripts.py
+++ batfish/snapshots/scripts.py
@@ -1,4 +1,4 @@
-def enum_tr_flows(tr_answer):
+def pprint_tr_flows(tr_answer):
 	for n, flow in enumerate(tr_answer['Flow']):
 	    print("============{}============".format(flow))
 	    for hop in tr_answer['Traces'][n]:
